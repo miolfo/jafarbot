d95d293c2c975148fff3fffb3aca628644746e2f
miolfo <mikko.o.forsman@gmail.com>
2018-08-15 19:15:54 +0300

    IrcConnect uses configuration properly

--- src/IrcConnect.ts
+++ src/IrcConnect.ts
@@ -1,12 +1,13 @@
 import * as fs from 'fs'
 import * as irc from 'irc'
+import JbConfig from './JbConfig';
 
 export default class IrcConnect {
-  
+
   public Connect () {
-    const oauthToken = fs.readFileSync('.oauthtoken', 'utf8');
-    let client = new irc.Client('irc.chat.twitch.tv', 'JafarinLeukaBot',{
-      password: oauthToken,
+    let config = new JbConfig();
+    let client = new irc.Client(config.getIrcServer(), config.getNick(),{
+      password: config.getOauthToken(),
       debug: true,
       autoConnect: false
     });
@@ -17,8 +18,8 @@ export default class IrcConnect {
 
     client.connect(1, function(msg) {
       client.send('CAP REQ', 'twitch.tv/membership');
-      client.join('#jafarinleuka', function(msg){
-        client.say('#jafarinleuka', 'hello world!');
+      client.join('#' + config.getChannel(), function(msg){
+        client.say('#' + config.getChannel(), 'hello world!');
         client.addListener('message', function (from, to, message) {
           console.log(from + ' => ' + to + ': ' + message);
       });
